2a505d71da4d6d9b0fa02cfcf6df7207eaaadc59
monali mayaskar <snehanatu@yahoo.com>
2019-01-28 10:47:04 -0500

    Added more fields in the model

--- src/app/models/music.models.ts
+++ src/app/models/music.models.ts
@@ -2,11 +2,15 @@ class Music {
     _id:string;
     songName: string;
     singerName: string;
+    albumName: string;
+    trackNumber: string;
     status: string;
 
     constructor(){
         this.songName = ""
         this.singerName = ""
+        this.albumName = ""
+        this.trackNumber = ""
         this.status = ""
     }
 }
